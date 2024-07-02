c85e0118b7a75c6f63233b12b573e6747f0d859e
tkucycc <145464164+tkucycc@users.noreply.github.com>
2024-07-02 11:57:22 +0800

    我的第一支java

---
+++ ch2_1.java
@@ -0,0 +1,14 @@
+//Ch2_1,簡單的java程式
+public class ch2_1 { //定義public 類別Ch2_1
+    public static void main(String[] args) {  //main()函數,主程式開始
+        int num1=2; //宣告整數num
+        int num2=3;
+        float num3=4;
+        String num4="五";
+        System.out.println(num1+"cat are running"); //印出字串及變數內容
+        System.out.println(num2+"dog are running");
+        System.out.println(num3+"birds are running");
+        System.out.println(num4+"tigers are running");
+    }
+}
+
