a650989f7f6318c691220365053f03e24742ffc4
tkucycc <412631086@o365.tku.edu.tw>
2024-07-03 11:32:19 +0800

    Create Ch2_4.java

---
+++ Ch2_4.java
@@ -0,0 +1,8 @@
+public class Ch2_4 {//有錯誤的程式(已修正)
+    public static void main(String[] args) {
+        int num1=2; //宣告整數變數num1，並設值為2
+        int num2=3; //宣告整數變數num2，並設值為3
+        System.out.println("I have"+num1+" dog");
+        System.out.println("You have"+num2+" dog");
+    }
+}
