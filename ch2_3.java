49442533e9f0ce2ff919021f1aa88ab80d553f3b
tkucycc <412631086@o365.tku.edu.tw>
2024-07-03 11:09:43 +0800

    Update ch2_3.java

--- ch2_3.java
+++ ch2_3.java
@@ -1,7 +1,8 @@
-public class ch2_3 {
+//使用println()函數
+public class ch2_3 { //定義類別Ch2_3
     public static void main(String[] args) {
-        int a=2,b=5;
-        System.out.println(a+"*"+b+"="+(a*b));
+        int a=2,b=5; //宣告變數a和b
+        System.out.println(a+"*"+b+"="+(a*b)); //印出運算結果
     }
     
 }
