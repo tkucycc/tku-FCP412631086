6aa7161c14f8ea118b570f299d423444d1c5b14b
tkucycc <412631086@o365.tku.edu.tw>
2024-07-03 11:43:51 +0800

    Create Ch2_6.java

---
+++ Ch2_6.java
@@ -0,0 +1,10 @@
+public class Ch2_6 { //定義public類別ch2_6
+    public static void main(String[] args) {
+        int n1=12;
+        int n2=26;
+        System.out.println(n1+"+"+n2+"="+(n1+n2));//印出n1和n2值
+        System.out.println("n1+n2="+(n1+n2));//印出n1+n2=38
+        System.out.println((n1+n2));//印出38
+    }
+    
+}
