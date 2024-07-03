80ff7b0f34bc74230839c71944fe7f424b5ba478
tkucycc <412631086@o365.tku.edu.tw>
2024-07-03 11:30:02 +0800

    Create Ch2_5.java

---
+++ Ch2_5.java
@@ -0,0 +1,8 @@
+public class Ch2_5 {
+    public static void main(String[] args) {
+        int a=2; //宣告整數變數a，並設值為a
+        int b=3;//宣告整數值為b，並設值為3
+        int c=a+b;
+        System.out.println("a+b="+c);
+    }
+}
