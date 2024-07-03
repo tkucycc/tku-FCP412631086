45b23c254f026660f77492864d0dd5be32a44e2c
tkucycc <412631086@o365.tku.edu.tw>
2024-07-03 11:03:22 +0800

    Update ch2_1.java

--- ch2_1.java
+++ ch2_1.java
@@ -1,8 +1,9 @@
 //Ch2_1,簡單的java程式
 public class ch2_1 { //定義public 類別Ch2_1
     public static void main(String[] args) {  //main()函數,主程式開始
-        int num1=2; //宣告整數num
-        int num2=3;
+        int num1,num2; //宣告整數num1,num2 為正整數
+        num1=1;//給值
+        num2=2;//給值
         float num3=4;
         String num4="五";
         System.out.println(num1+"cat are running"); //印出字串及變數內容
