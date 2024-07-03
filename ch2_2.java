31264bc74f74a424c366e8e963aeee7d6a49c8b7
tkucycc <412631086@o365.tku.edu.tw>
2024-07-03 11:02:13 +0800

    Create ch2_2.java

---
+++ ch2_2.java
@@ -0,0 +1,9 @@
+public class ch2_2 { //定義為別Ch2_2
+    public static void main(String[] args) {
+        int a;//宣告半數a，但不設定初值
+        int b=4,sum;//宣告b和sum兩個變數，但只將的值設為4
+        a=5;//設定a值為5
+        sum=a+b;//將sum的值設為a+b
+        System.out.println("sum="+sum);//印出sum值
+    }
+}
