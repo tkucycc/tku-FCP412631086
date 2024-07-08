87be2b493dbece42a14bd5e9702b5403178a05d3
tkucycc <412631086@o365.tku.edu.tw>
2024-07-08 11:09:22 +0800

    0708作業

---
+++ 正課/ch3_1.java
@@ -0,0 +1,10 @@
+package 正課;
+
+public class ch3_1 {
+    public static void main(String[] args) {
+        int num=3;
+        char ch='C';
+        System.out.println(num+" is an integer");
+        System.out.println(ch+" is a charcter");
+    }
+}
---
+++ 正課/ch3_3.java
@@ -0,0 +1,15 @@
+package 正課;
+
+public class ch3_3 {
+    public static void main(String[] args) {
+        byte bin =0b1111;
+        short oct=02001;
+        int hex1=0xff12;
+        long hex2=0x12345ffeL;
+        
+        System.out.println("bin="+bin);
+        System.out.println("oct="+oct);
+        System.out.println("hex1="+hex1);
+        System.out.println("hex2="+hex2);
+    }
+}
