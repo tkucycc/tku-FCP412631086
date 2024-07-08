10a2a0446669d3093caeabbd57dc705099335b64
tkucycc <412631086@o365.tku.edu.tw>
2024-07-08 14:42:42 +0800

    0708作業

---
+++ 正課/hm_06.java
@@ -0,0 +1,16 @@
+package 正課;
+import java.util.Scanner;
+public class hm_06 {
+    public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    System.out.print("輸入一個整數");  
+    int n1 = scn.nextInt();
+    System.out.print("輸入第二個整數");
+    int n2 = scn.nextInt();
+    int a=n1+n2;
+    System.out.println(a);
+    scn.close();
+    }
+
+    
+}
