dfa23a69f294a3b9b8b6b9f0f162fd00fbe66c2f
tkucycc <412631086@o365.tku.edu.tw>
2024-07-09 13:56:06 +0800

    0709作業

---
+++ 正課/ch5_3.java
@@ -0,0 +1,10 @@
+package 正課;
+
+public class ch5_3 {
+    public static void main(String[] args) {
+        int a=8,b=3 ,max;
+
+        max=(a>b)?a:b;
+        System.out.printf("a=%d,b=%d,%d是較大的\n",a,b,max);
+    }
+}
